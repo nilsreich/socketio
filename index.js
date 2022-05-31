2b1c0512cd116b8ebe44d7c0212397425faa3434
nilsreich <nilsreich@gmail.com>
2022-05-31 06:40:26 +0000

    codespace ports must be public

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "https://nilsreich-socketio-x5qjg5g36wgv-3000.githubpreview.dev",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
 const PORT = process.env.PORT || 5000;
 
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname+'/index.html');
 });
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
       io.emit('chat message', msg);
+      console.log(msg)
     });
   });
 
--- src/App.js
+++ src/App.js
@@ -6,17 +6,40 @@ function App() {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io();
+    const newSocket = io(
+      "https://nilsreich-socketio-x5qjg5g36wgv-5000.githubpreview.dev",
+      {
+        withCredentials: true,
+        extraHeaders: {
+          "my-custom-header": "abcd",
+        },
+      }
+    );
     setSocket(newSocket);
     return () => newSocket.close();
   }, [setSocket]);
+
+  useEffect(() => {
+    if(socket){
+      socket.on("chat message", function (msg) {
+        console.log(msg);
+      });
+    }
+  }, [socket]);
+
+  const [eingabe, setEingabe] = useState("");
+
   return (
     <div className="App">
       <ul id="messages"></ul>
-      <form id="form" action="">
-        <input id="input" autoComplete="off" />
-        <button onClick={()=>{console.log('hi')}}>Send</button>
-      </form>
+      <input value={eingabe} onChange={(e) => setEingabe(e.target.value)} />
+      <button
+        onClick={() => {
+          socket.emit("chat message", eingabe);
+        }}
+      >
+        Send
+      </button>
     </div>
   );
 }
